75bc22601c0232f1ad39bcb9dc9851c4313f4980
Kunti <kuntishah@gmail.com>
2017-09-27 21:11:54 -0700

    Update link name

--- extractlinkstofiles.py
+++ extractlinkstofiles.py
@@ -16,11 +16,12 @@ link_list = list()
 def clean_link(old_link):
     #"javascript:load_tab_content('/classifications/nice/nicepub/en/fr/edition-20170101/taxonomy/class-35/')
     #extract only the link from the above text
-    pos1 = old_link.find("'")
-    pos2 = old_link.find("'",pos1+1)
-    new_link = old_link[pos1+1:pos2]
+##    pos1 = old_link.find("'")
+##    pos2 = old_link.find("'",pos1+1)
+##    print pos1, pos2
+##    new_link = old_link[pos1+1:pos2]
     #add "web2.wipo.int" to the link to create the new_link
-    new_link = "http://web2.wipo.int" + new_link
+    new_link = "http://web2.wipo.int" + old_link
     #print new_link
     return new_link
 
@@ -31,15 +32,18 @@ tags = page_soup('td')
 for tag in tags:
  #   class_num = tag.text
     findA = tag.find('a')
-    print findA
+    #print findA
     if findA is not None:
         href = findA.get('href',None)
-        print href
+        #print href
         link_list.append(clean_link(href))
 
+
 count = 1
+
 for link in link_list:
     filename = "class" + str(count) + ".html"
     page = urllib.urlretrieve(link,filename)
     count = count + 1
+    
     
